7952aa78f1d0d73a69dcace866e844d82b7dcd26
Mark Piper <mark.piper@colorado.edu>
2017-01-24 12:04:53 -0700

    Update unit tests

--- bmi_ilamb/tests/test_run.py
+++ bmi_ilamb/tests/test_run.py
@@ -1,15 +1,27 @@
-from nose.tools import raises
-from bmi_ilamb.cmd.run import main
+import os
+import subprocess
+from nose.tools import raises, assert_equal
+from .. import ILAMB_ROOT
+from bmi_ilamb.cmd.run import parse_arguments, main
 
 
-_HERE = path.abspath(path.dirname(__file__))
+cmd = 'ilamb2-run'
 
 
 def setup_module():
     """Fixture called before any tests are performed."""
     print('\n*** ' + __name__)
-        
+    try:
+        os.environ['ILAMB_ROOT']
+    except KeyError:
+        os.environ['ILAMB_ROOT'] = ILAMB_ROOT
 
-@raises(SystemExit)
-def test_run_fails_with_no_parameters():
-    main()
+
+def test_help_argument():
+    r = subprocess.call([cmd, '--help'])
+    assert_equal(r, 0)
+
+
+@raises(subprocess.CalledProcessError)
+def test_fails_without_config_file():
+    r = subprocess.check_call([cmd])
