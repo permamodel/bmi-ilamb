8bdf671af72d36ccdc606132dd96be47d03cc369
Mark Piper <mark.piper@colorado.edu>
2017-01-24 11:09:45 -0700

    Add BmiIlamb class

---
+++ bmi_ilamb/bmi_ilamb.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+import sys
+import subprocess
+
+
+class BmiIlamb(object):
+    _command = 'ilamb2-run'
+    _args = None
+    _env = None
+
+    def __init__(self):
+        self._time = self.get_start_time()
+
+    @property
+    def args(self):
+        return [self._command] + (self._args or [])
+
+    def get_component_name(self):
+        return 'ILAMB v2'
+
+    def initialize(self, filename):
+        self._args = [filename or 'ilamb.cfg']
+
+    def update(self):
+        subprocess.check_call(self.args, shell=False, env=self._env)
+        self._time = self.get_end_time()
+
+    def update_until(self, time):
+        self.update(time)
+
+    def finalize(self):
+        pass
+
+    def get_input_var_names(self):
+        return ()
+
+    def get_output_var_names(self):
+        return ()
+
+    def get_start_time(self):
+        return 0.0
+
+    def get_end_time(self):
+        return 1.0
+
+    def get_current_time(self):
+        return self._time
+
+    def get_time_step(self):
+        return 1.0
+
+    def get_time_units(self):
+        return 's'
