30bc5023b78bd45a188bac7433e63c6d7f301149
Mark Piper <mark.piper@colorado.edu>
2017-02-07 15:24:39 -0700

    Create data_dir variable

--- bmi_ilamb/__init__.py
+++ bmi_ilamb/__init__.py
@@ -1,3 +1,4 @@
+import os
 from .bmi_ilamb import BmiIlamb
 
 
@@ -5,3 +6,6 @@ __all__ = ['BmiIlamb']
 __version__ = 0.1
 
 ILAMB_ROOT='/nas/data/ilamb'  # location on beach
+
+package_dir = os.path.dirname(__file__)
+data_dir = os.path.join(package_dir, 'data')
