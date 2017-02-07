737b4a374dc281303f40b0e83f1073f5397402e0
Mark Piper <mark.piper@colorado.edu>
2017-02-07 15:58:20 -0700

    Remove ILAMB_ROOT variable
    Instead, specify it in the BMI config file.

--- bmi_ilamb/__init__.py
+++ bmi_ilamb/__init__.py
@@ -5,7 +5,5 @@ from .bmi_ilamb import BmiIlamb
 __all__ = ['BmiIlamb']
 __version__ = 0.1
 
-ILAMB_ROOT='/nas/data/ilamb'  # location on beach
-
 package_dir = os.path.dirname(__file__)
 data_dir = os.path.join(package_dir, 'data')
