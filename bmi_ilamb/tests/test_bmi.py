193705e99e6259d11c4871e4f24c7a75b499b020
Mark Piper <mark.piper@colorado.edu>
2017-02-09 13:32:26 -0700

    Add unit tests for IRF methods

--- bmi_ilamb/tests/test_bmi.py
+++ bmi_ilamb/tests/test_bmi.py
@@ -1,5 +1,13 @@
-from nose.tools import assert_is, assert_equal
+"""Unit tests for the ILAMB BMI."""
+
+import os
+from nose.tools import raises, assert_is, assert_equal, assert_true
 from bmi_ilamb import BmiIlamb
+from .. import data_dir
+
+
+bmi_ilamb_config = os.path.join(data_dir, 'bmi_ilamb.yaml')
+
 
 def test_component_name():
     component = BmiIlamb()
@@ -43,4 +51,44 @@ def test_get_output_var_names():
     assert_equal(component.get_output_var_names(), ())
 
 
-# Todo: test initialize, update, update_until, finalize.
+@raises(TypeError)
+def test_initialize_no_argument():
+    component = BmiIlamb()
+    component.initialize()
+
+
+def test_initialize():
+    component = BmiIlamb()
+    component.initialize(bmi_ilamb_config)
+
+
+def test_initialize_n_arguments():
+    component = BmiIlamb()
+    component.initialize(bmi_ilamb_config)
+    assert_equal(len(component.args), 7)
+
+
+def test_initialize_sets_env_vars():
+    component = BmiIlamb()
+    component.initialize(bmi_ilamb_config)
+    assert_true(os.environ.has_key('ILAMB_ROOT'))
+    assert_true(os.environ.has_key('MPLBACKEND'))
+
+
+def test_finalize():
+    component = BmiIlamb()
+    component.finalize()
+
+
+def test_update():
+    component = BmiIlamb()
+    component.initialize(bmi_ilamb_config)
+    component.update()
+    component.finalize()
+
+
+def test_update_until():
+    component = BmiIlamb()
+    component.initialize(bmi_ilamb_config)
+    component.update_until(10.0)
+    component.finalize()
