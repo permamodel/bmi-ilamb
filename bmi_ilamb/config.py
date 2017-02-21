38194c76023787b887c1250b9f1deb9f4428acfa
Mark Piper <mark.piper@colorado.edu>
2017-02-20 17:17:40 -0700

    Let model_root be specified relative to ilamb_root

--- bmi_ilamb/config.py
+++ bmi_ilamb/config.py
@@ -1,9 +1,11 @@
 """Reads and parses a configuration file for the ILAMB BMI."""
 
+from os.path import join
 import yaml
 
 
 ilamb_root_key = 'ilamb_root'
+model_root_key = 'model_root'
 
 
 class Configuration(object):
@@ -18,8 +20,14 @@ class Configuration(object):
     def get_ilamb_root(self):
         return self._config.get(ilamb_root_key)
 
+    def _set_model_root(self):
+        rel = self._config.get(model_root_key)
+        if rel is not None:
+            self._config[model_root_key] = join(self.get_ilamb_root(), rel)
+
     def get_arguments(self):
         args = []
+        self._set_model_root()
         for k, v in self._config.iteritems():
             if k != ilamb_root_key:
                 args.append('--' + k)
